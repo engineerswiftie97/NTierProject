81a4432ad39a0ae92918659e0dedc35c19812e72
melisaakbaydar <51860635+melisaakbaydar@users.noreply.github.com>
2021-02-04 00:23:55 +0300

    Add project files.

---
+++ Project.API/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.API.Controllers
+{
+    public class CategoryController
+    {
+    }
+}
---
+++ Project.WEBUI/Models/PAVM.cs
@@ -0,0 +1,19 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WEBUI.Models
+{
+    public class PAVM
+    {
+        public Product Product { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        //Todo: Pagination işlemleri (Alısveris)
+    }
+}
